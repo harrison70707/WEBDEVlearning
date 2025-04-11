b16114a3bf0d4a4a9a652c229c486d62890e0cb3
Harrison Gough <hgough1@wgu.edu>
2025-04-11 11:11:44 -0500

    add event listners to .drum buttons

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,6 +1,12 @@
-document.querySelector("button").addEventListener("click", handleClick);
 
-
-function handleClick() {
-    alert("I got clicked");
+for (var i=0; i<document.querySelectorAll(".drum").length; i++){
+    document.querySelectorAll("button")[i].addEventListener("click", function() {
+        alert("I got clicked");
+    });
 }
+
+
+
+
+
+
